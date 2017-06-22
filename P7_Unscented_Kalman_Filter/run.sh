f586541ed7d727e560964548b7ca67c15f447f44
Rakesh Dhote <rakesh.dhote@gmail.com>
2017-06-21 22:53:29 -0400

    updated run script to redirect output to different files

--- P7_Unscented_Kalman_Filter/run.sh
+++ P7_Unscented_Kalman_Filter/run.sh
@@ -2,6 +2,6 @@ rm -rf build
 mkdir build && cd build
 cmake .. && make
 # echo "~~~ Data 1 ~~~"
-./UnscentedKF ../data/sample-laser-radar-measurement-data-1.txt output.txt
+./UnscentedKF ../data/sample-laser-radar-measurement-data-1.txt output1.txt
 # echo "~~~ Data 2 ~~~"
-./UnscentedKF ../data/sample-laser-radar-measurement-data-2.txt output.txt
+./UnscentedKF ../data/sample-laser-radar-measurement-data-2.txt output2.txt
